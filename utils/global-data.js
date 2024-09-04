a69c8e2808e33e0b1b2013fb7f4f80cccc03b95f
absaman <aabsalan26@gmail.com>
2024-09-04 19:07:16 +1000

    renamed custom link.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Welcome!';
+    : 'Zero Day Diaries';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Zero-Day Diaries';
